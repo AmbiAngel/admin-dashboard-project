ee23df016ec3dfed389a0c33ebad86ff42c081e2
Angel <angelambisilva@gmail.com>
2023-09-14 21:23:53 -0400

    add svg icons for dashboard section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./style.css"
 import otto from './imgs/cool-otto.png';
 
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  const images = importAll(require.context('./imgs/icons', false, /\.(png|jpe?g|svg)$/));
